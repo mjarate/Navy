a113942e4c9816354981f67b6f23861b040cb597
Simon AM <simon@agius-muscat.net>
2015-08-14 15:28:18 +0200

    Added first module

--- Navy/navy_modules.h
+++ Navy/navy_modules.h
@@ -1,1 +1,32 @@
 #include "navy_macros.h"
+
+class Logic;
+class Module_F: Logic {
+    class ModuleDescription {};
+};
+
+class Navy_Module: Module_F {
+    scope = 1;
+    displayName = "Navy Module";
+    icon = ADDON_PATH(resources\navy.paa);
+    category = "Navy";
+    function = "";
+    functionPriority = 1;
+    isTriggerActivated = 0;
+    isGlobal = 0;
+    isDisposable = 0;
+    vehicleClass = "Modules";
+    class ModuleDescription: ModuleDescription {
+        description = "Navy Module";
+    };
+};
+
+class Navy_Paradrop: Navy_Module {
+    scope = 2;
+    condition = "navy_isEnabled";  // add config check
+    displayName = "Paradrop";
+    function = "navy_module_paradrop";
+    class ModuleDescription {
+        description = "Spawns a unit paradrop";
+    };
+};
