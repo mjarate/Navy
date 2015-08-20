8c7a955dfed4bcc31c23c755efbca15c5a45fb8b
Simon AM <simon@agius-muscat.net>
2015-08-20 12:08:23 +0200

    Fixed routines config formatting

--- Navy/navy_routines.h
+++ Navy/navy_routines.h
@@ -6,43 +6,43 @@ class Routines {
         waypoint_type[] = {};
         waypoint_behaviour[] = {};
         waypoint_combatmode[] = {};
-        waypoint_statements[] = {{"",""},{"",""},{"",""}};
+        waypoint_statements[] = {{"",""}, {"",""}, {"",""}};
     };
 
     class Loiter : Default {
         flight_height = 250;
         waypoint_count = 1;
-        waypoint_speed[] = {"LIMITED"};
-        waypoint_type[] = {"LOITER"};
-        waypoint_behaviour[] = {"AWARE"};
-        waypoint_combatmode[] = {"RED"};
+        waypoint_speed[] = {"", "", "LIMITED"};
+        waypoint_type[] = {"", "", "LOITER"};
+        waypoint_behaviour[] = {"", "", "AWARE"};
+        waypoint_combatmode[] = {"", "", "RED"};
     };
 
     class CAS_Patrol : Default {
         flight_height = 250;
         waypoint_count = 3;
-        waypoint_speed[] = {"LIMITED","LIMITED","LIMITED"};
-        waypoint_type[] = {"SAD","SAD","SAD"};
-        waypoint_behaviour[] = {"AWARE","AWARE","AWARE"};
-        waypoint_combatmode[] = {"RED","RED","RED"};
-        waypoint_statements[] = {{"",""},{"",""},{"true","(group this) setCurrentWaypoint [group this,1]"}};
+        waypoint_speed[] = {"LIMITED", "LIMITED", "LIMITED"};
+        waypoint_type[] = {"SAD", "SAD", "SAD"};
+        waypoint_behaviour[] = {"AWARE", "AWARE", "AWARE"};
+        waypoint_combatmode[] = {"RED", "RED", "RED"};
+        waypoint_statements[] = {{"",""}, {"",""}, {"true","(group this) setCurrentWaypoint [group this,1]"}};
     };
 
     class Heli_Insert : Default {
         flight_height = 100;
         waypoint_count = 2;
-        waypoint_speed[] = {"NORMAL","FULL"};
-        waypoint_type[] = {"MOVE","MOVE"};
-        waypoint_behaviour[] = {"CARELESS","AWARE"};
-        waypoint_combatmode[] = {"BLUE","BLUE"};
+        waypoint_speed[] = {"NORMAL", "FULL"};
+        waypoint_type[] = {"MOVE", "MOVE"};
+        waypoint_behaviour[] = {"CARELESS", "AWARE"};
+        waypoint_combatmode[] = {"BLUE", "BLUE"};
     };
 
     class Paradrop : Default {
         flight_height = 200;
         waypoint_count = 2;
-        waypoint_speed[] = {"NORMAL","NORMAL"};
-        waypoint_type[] = {"MOVE","MOVE"};
-        waypoint_behaviour[] = {"AWARE","AWARE"};
-        waypoint_combatmode[] = {"BLUE","BLUE"};
+        waypoint_speed[] = {"", "NORMAL", "NORMAL"};
+        waypoint_type[] = {"", "MOVE", "MOVE"};
+        waypoint_behaviour[] = {"", "AWARE", "AWARE"};
+        waypoint_combatmode[] = {"", "BLUE", "BLUE"};
     };
 };
