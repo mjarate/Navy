77a82131f4a98b83196dad77f2433ca65c41d65e
Simon AM <simon@agius-muscat.net>
2015-08-25 10:39:03 +0200

    Version set to 0.2.1

--- Navy/navy_macros.h
+++ Navy/navy_macros.h
@@ -1,7 +1,7 @@
 #ifndef NAVY_MACROS_H
 #define NAVY_MACROS_H
 
-#define NAVY_VERSION_STR             "0.2.0"
+#define NAVY_VERSION_STR             "0.2.1"
 
 #define CONCAT_ADDON_PATH(FILE)         x\ark\addons\navy\FILE
 #define ADDON_PATH(FILE)                #CONCAT_ADDON_PATH(FILE)
